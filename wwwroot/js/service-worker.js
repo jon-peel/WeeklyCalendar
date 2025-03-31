4a96ed904b3eb6692b76eb43416b0bdbe13be677
Jonathan Peel <me@jonathanpeel.co.za>
2025-03-31 21:00:10 +0300

    fix: Add newline at end of service-worker.js for consistency

--- wwwroot/js/service-worker.js
+++ wwwroot/js/service-worker.js
@@ -13,9 +13,11 @@ self.addEventListener('install', event => {
   );
 });
 
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
 });
+ 
